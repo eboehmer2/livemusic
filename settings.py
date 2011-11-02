e321981fd2bf6084d60600ff935a3d918b5c59b7
Eileen Boehmer <eileen.chalupa@gmail.com>
2011-11-01 21:06:35 -0500

    5th commit

--- settings.py
+++ settings.py
@@ -78,6 +78,8 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'livemusic.urls'
 
 TEMPLATE_DIRS = (
+    '/home/littlechalupa/livemusic/templates/admin',
+    '/home/littlechalupa/livemusic/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- shows/views.py
+++ shows/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from livemusic.shows.models import Venue, Band, Event
+from django.shortcuts import render_to_response
+
+def homepage(request):
+    events = Event.objects.order_by('event_date')
+    return render_to_response('homepage.html', {
+        'events': events,
+    })
