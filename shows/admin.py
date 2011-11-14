929034d871b4e90106b8fe765e818975afc37956
Eileen Boehmer <eileen.chalupa@gmail.com>
2011-11-13 23:12:42 -0600

    reworked models to include ManyToManyFields instead of ForeignKeys and updated templates accordingly - slightly broken still

--- shows/admin.py
+++ shows/admin.py
@@ -1,8 +1,10 @@
 from shows.models import Venue
 from shows.models import Event
 from shows.models import Band
+from shows.models import Genre
 from django.contrib import admin
 
 admin.site.register(Venue)
 admin.site.register(Event)
 admin.site.register(Band)
+admin.site.register(Genre)
--- shows/models.py
+++ shows/models.py
@@ -1,38 +1,13 @@
 from django.db import models
 
-GENRE_CHOICES = (
-    ('acoustic', "Acoustic"),
-    ('alternative', "Alternative"),
-    ('bluegrass', "Bluegrass"),
-    ('blues', "Blues"),
-    ('country', "Country"),
-    ('electronic', "Electronic & Techno"),
-    ('folk', "Folk"),
-    ('funk', "Funk"),
-    ('gospel', "Gospel & Religious"),
-    ('hip hop', "Hip Hop & Rap"),
-    ('indie', "Indie"),
-    ('instrumental', "Instrumental"),
-    ('jazz', "Jazz"),
-    ('latin', "Latin"),
-    ('metal', "Metal"),
-    ('pop', "Pop"),
-    ('punk', "Punk"),
-    ('r&b', "R&B"),
-    ('reggae', "Reggae"),
-    ('rock', "Rock"),
-    ('rock-n-roll', "Rock-n-Roll"),
-    ('soul', "Soul"),
-    ('world', "World"),
-    ('other', "Other"),
-    ('n_a', "N/A"),
-)
 
 COVER_CHOICES = (
     ('0', "$0"),
     ('1-4', "$1-4"),
     ('5-9', "$5-9"),
-    ('10+', "$10 or more"),
+    ('10-14', "$10-14"),
+    ('15-19', "$15-19"),
+    ('20+', "$20 or more"),
     ('unknown', "Unknown"),
 )
 
@@ -50,11 +25,16 @@ class Venue(models.Model):
     def __unicode__(self):
         return self.venue_name
 
+class Genre(models.Model):
+    genre = models.CharField(max_length=50)
+    def get_absolute_url(self):
+        return "/genres/%i/" % self.id
+    def __unicode__(self):
+        return self.genre
+
 class Band(models.Model):
     band_name = models.CharField(max_length=200)
-    genre_1 = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    genre_2 = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    genre_3 = models.CharField(max_length=50, choices=GENRE_CHOICES)
+    band_genres = models.ManyToManyField(Genre)
     band_website = models.CharField(max_length=200)
     def get_absolute_url(self):
         return "/bands/%i/" % self.id
@@ -65,14 +45,10 @@ class Event(models.Model):
     event_name = models.CharField(max_length=200)
     event_date = models.DateField('event date')
     start_time = models.TimeField('event time')
-    band_name_1 = models.ForeignKey(Band, related_name='event_band_name_1')
-    band_name_2 = models.ForeignKey(Band, related_name='event_band_name_2')
-    band_name_3 = models.ForeignKey(Band, related_name='event_band_name_3')
+    band_names = models.ManyToManyField(Band)
     cover = models.CharField(max_length=20, choices=COVER_CHOICES)
-    genre_1 = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    genre_2 = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    genre_3 = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    venue_name = models.ForeignKey(Venue, related_name='event_venue_name')
+    event_genres = models.ManyToManyField(Genre)
+    venue_name = models.ForeignKey(Venue)
     def get_absolute_url(self):
         return "/events/%i/" % self.id
     def __unicode__(self):
